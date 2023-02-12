26551bdab25ffb83e1463066bc6ff84c5ffc86bc
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-02-12 18:41:57 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+import { useState, useEffect } from "react";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
-    fetch('https://reqres.in/api/users')
-     .then(res => res.json())
-     .then(json => {
-      setUsers(json.data)
-     })
-     .catch(err => {
-      console.war(err);
-      alert('Ошибка при получении пользователя')
-     }) 
-  })
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.war(err);
+        alert("Ошибка при получении пользователя");
+      })
+      .finally(() => setLoading(false));
+  });
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onClickInvite={onClickInvite}
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
